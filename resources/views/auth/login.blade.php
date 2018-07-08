9d59a4ec01374a8d22fa7c50ba7e2ce7f12a1f16
ugikmat <ugik.mats@gmail.com>
2018-07-08 18:58:18 +0700

    Change auth with username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,13 @@
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
+                                {{-- <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus> --}}
+                                <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" autofocus>
+                                {{-- @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
 
